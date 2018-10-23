eab2acdd9a1ff457edb734ba29d9dd304197a50e
1computerguy <31444089+1computerguy@users.noreply.github.com>
2018-10-23 13:24:32 -0400

    Add files via upload

---
+++ Add-Users.ps1
@@ -0,0 +1,33 @@
+<#
+
+** users.csv file format **
+Name,SurName,Domain,Path,Password,Groups
+BCT,S2,3bct4id.army.mil,"OC=CQ-Battalion,OU=3BCT4ID Service Accounts,DC=3BCT4ID,DC=ARMY,DC=MIL",345rtyui#$%RTYUI,"Group 1,Group 2,Group 3"
+
+
+#>
+
+import-module ActiveDirectory
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+$users = "C:\users\bccsadministrator\Documents\users.csv"
+
+import-csv $users | % {
+	$acct = "$($_.Name)`.$($_.SurName)"
+	$full = "$($_.Name)` $($_.SurName)"
+    $email = "$($acct)`@$($_.Domain)"
+	New-AdUser -Name $full -Surname $_.SurName -GivenName $_.Name -DisplayName $full -samAccountName $acct -AccountPassword $(ConvertTo-SecureString $_.Password -AsPlainText -Force) -Path $_.path -Enable $True -OtherAttributes @{mail=$email} -PasswordNeverExpires $True -CannotChangePassword $True -UserPrincipalName $email -PassThru
+
+	($_.Groups).split(",") | % { Add-AdGroupMember -Identity $_ -Member $acct }
+
+    # Check AD for creation of user and propogation before creating email Accounts
+    # If Email creation line runs before account has propogated, then it will fail
+    while (-not $AdUser0 {
+        try {
+            $AdUser = Get-AdUser $acct.Trim()
+        } catch {
+            sleep 10
+        }
+    }
+    Enable-Mailbox -Identity $email -Alias $acct -DisplayName $full
+}
